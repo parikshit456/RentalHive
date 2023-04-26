296e77492fefadaa14a01ee68666fb926cdaac8d
Rohit5464 <rohit.work5464@gmail.com>
2023-04-26 17:18:01 +0530

    rohit-commit

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,13 +1,33 @@
+
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB1n_WjA4VtcFFUIlF-zZZZPgqbRg8ohlE",
+//   authDomain: "house-hunter-5e5ce.firebaseapp.com",
+//   projectId: "house-hunter-5e5ce",
+//   storageBucket: "house-hunter-5e5ce.appspot.com",
+//   messagingSenderId: "747293056462",
+//   appId: "1:747293056462:web:261be3fb1c4bb911d0df9f"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore()
+
 // Import the functions you need from the SDKs you need
+import { getFirestore } from "@firebase/firestore";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB1n_WjA4VtcFFUIlF-zZZZPgqbRg8ohlE",
-  authDomain: "house-hunter-5e5ce.firebaseapp.com",
-  projectId: "house-hunter-5e5ce",
-  storageBucket: "house-hunter-5e5ce.appspot.com",
-  messagingSenderId: "747293056462",
-  appId: "1:747293056462:web:261be3fb1c4bb911d0df9f"
+  apiKey: "AIzaSyC0ANYxOhJt2xuRV51agSGR4lqgYujI81U",
+  authDomain: "househunter-501c3.firebaseapp.com",
+  projectId: "househunter-501c3",
+  storageBucket: "househunter-501c3.appspot.com",
+  messagingSenderId: "544334896615",
+  appId: "1:544334896615:web:10223d9b334b39e163f142"
 };
 
 // Initialize Firebase
