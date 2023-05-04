00bec73937807c7e31e351b2ad80a2e6594ec5a3
Rohit5464 <rohit.work5464@gmail.com>
2023-05-04 12:37:20 +0530

    rohii-commit

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,30 +1,96 @@
-import React from 'react';
-import dhairya from "../../src/assets/jpg/dhairya.jfif"
-import parikshit from '../../src/assets/jpg/parikshit.jpg'
-import rohit from '../../src/assets/jpg/rohit.jpg'
-import Footer from './Footer';
+import React from "react";
+import dhairya from "../../src/assets/jpg/dhairya.jfif";
+import parikshit from "../../src/assets/jpg/parikshit.jpg";
+import rohit from "../../src/assets/jpg/rohit.jpg";
+import Footer from "./Footer";
+import github from "../assets/svg/github.svg";
+import linkedin from "../assets/svg/linkedin.svg";
+import facebook from "../assets/svg/facebook.svg";
+import instagram from "../assets/svg/instagram.svg";
+
 function AboutUs() {
   return (
-    <div className="about-us-container">
-      <div className="about-person">
-        <img src={dhairya} alt="Person 1" className='ani' />
-        <h3>Dhairya Parikh</h3>
-      
-        <a href="https://www.linkedin.com/in/person1/" target="_blank" rel="noreferrer">LinkedIn Profile</a>
+    <div>
+      <div className="about-us-container">
+        <div className="about-person">
+          <img src={dhairya} alt="Person 1" className="ani" />
+          <h3 className="animate-charcter">Dhairya Parikh</h3>
+
+          <div style={{display:"flex" , justifyContent:"center"}}>
+            <a>
+              <img
+                src={instagram}
+                className="about-img"
+              />
+            </a>
+            <a>
+              <img
+                src={github}
+                className="about-img" style={{height:"50px" ,width:"50px" , marginTop:"0px"}}/>
+            </a>
+            <a>
+              <img
+                src={linkedin}
+                className="about-img"
+              />
+            </a>
+          </div>
+        </div>
+       
+        <div className="about-person">
+          <img src={rohit} alt="Person 3" className="ani" />
+          <h3 className="animate-charcter">Rohit Dubey</h3>
+
+          <div style={{display:"flex" , justifyContent:"center"}}>
+            <a>
+              <img
+                src={instagram}
+                className="about-img"
+              />
+            </a>
+            <a>
+              <img
+                src={github}
+                className="about-img" style={{height:"50px" ,width:"50px" , marginTop:"0px"}}/>
+            </a>
+            <a>
+              <img
+                src={linkedin}
+                className="about-img"
+              />
+            </a>
+            
+          </div>
+        </div>
+
+        <div className="about-person">
+          <img src={parikshit} alt="Person 2" className="ani" />
+          <h3 className="animate-charcter" >Parikshit Agarwal</h3>
+          <div>
+          <div style={{display:"flex" , justifyContent:"center"}}>
+            <a>
+              <img
+                src={instagram}
+                className="about-img"
+              />
+            </a>
+            <a>
+              <img
+                src={github}
+                className="about-img" style={{height:"50px" ,width:"50px" , marginTop:"0px"}}/>
+            </a>
+            <a>
+              <img
+                src={linkedin}
+                className="about-img"
+              />
+            </a>
+            
+          </div>
+        </div>
+        </div>
       </div>
-      <div className="about-person">
-        <img src={parikshit} alt="Person 2"  className='ani' />
-        <h3>Parikshit Agarwal</h3>
-   
-        <a href="https://www.linkedin.com/in/person2/" target="_blank" rel="noreferrer">LinkedIn Profile</a>
-      </div>
-      <div className="about-person">
-        <img src={rohit} alt="Person 3"  className='ani' />
-        <h3>Rohit Dubey</h3>
-   
-        <a href="https://www.linkedin.com/in/person3/" target="_blank" rel="noreferrer">LinkedIn Profile</a>
-      </div>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
